bfdcba4b11b439286b3d45d1fbf94064cf23bea4
Nitin <niting@axioned.com>
2021-11-11 17:06:50 +0530

    Menu Click Events Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
-/* Author: 
+/* Author: NDG */
 
-*/
+// Menu click Events
 
-
-
+// click Event on menu icon
+document.querySelector("nav").addEventListener("click", function(){
+    this.classList.toggle("nav-cross");
+    document.querySelector(".nav-list").classList.toggle("hide-me-mobile");
+    document.querySelector(".header-social-icons").classList.toggle("hide-me-mobile");
+});
+// click Event to stop Propagation
+document.querySelector(".nav-list").addEventListener("click", function(e){ e.stopPropagation(); });
 
 
 
