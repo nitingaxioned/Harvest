d62ef1789d73df5acef1743ce5d9dc1c04400fe0
Nitin <niting@axioned.com>
2021-11-11 20:46:44 +0530

    Filter JS as Page functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,62 +13,103 @@ document.querySelector("nav").addEventListener("click", function(){
 document.querySelector(".nav-list").addEventListener("click", function(e){ e.stopPropagation(); });
 
 
-// Latest Work Modal/LightBox
+// Filtering JS as Page
 
-var modal = document.querySelector(".latest-work-modal");
-// click Event on Latest Work list
-document.querySelectorAll(".portfolio-item").forEach(function(val){
-    val.addEventListener("click", function(){
-        modal.querySelector(".wrapper").innerHTML = "";
-        var tempNode = val.querySelector("figure").cloneNode();
-        tempNode.innerHTML = val.querySelector("figure").innerHTML;
-        modal.querySelector(".wrapper").appendChild(tempNode);
-        modal.classList.remove("hide-me");
-        // click event to to stope propagation
-        tempNode.addEventListener("click", function(e){ e.stopPropagation(); });
-        // click event to close modal
-        modal.addEventListener("click", function(){ this.classList.add("hide-me") });
-    });
-});
+// geting page titel
+var pageTitle = document.querySelector("title").innerHTML.replace("Harvest | ","");
+// setting JS of loaded page;
+if(pageTitle == "Home")
+    index();
+if(pageTitle == "Our Work")
+    ourWork();
+if(pageTitle == "Join Us")
+    joinUs();
 
 
-// Our work Filter functionality
+// Function For Index Page
 
-var portfolio_btns = document.querySelectorAll(".portfolio-btn a");
-var portfolio_list = document.querySelectorAll(".portfolio-item-img")
-// Click Event on Portfolio btns
-portfolio_btns.forEach(function(val){
-    val.addEventListener("click", function(){
-        // removing Active class 
-        portfolio_btns.forEach(function(value){value.classList.remove("active");});
-        // adding Active class 
-        val.classList.add("active");
-        var tempStr = val.innerHTML.toLowerCase().replace(" ","-");
-        // Filtering Nodes
-        portfolio_list.forEach(function(Img_Node){
-            if(val.innerHTML=="all")
-                Img_Node.classList.remove("hide-me")
-            else if( Img_Node.classList.contains(tempStr) )
-                Img_Node.classList.remove("hide-me");
-            else
-                Img_Node.classList.add("hide-me");
+function index(){
+    // Latest Work Modal/LightBox
+
+    var modal = document.querySelector(".latest-work-modal");
+    // click Event on Latest Work list
+    document.querySelectorAll(".portfolio-item").forEach(function(val){
+        val.addEventListener("click", function(){
+            modal.querySelector(".wrapper").innerHTML = "";
+            var tempNode = val.querySelector("figure").cloneNode();
+            tempNode.innerHTML = val.querySelector("figure").innerHTML;
+            modal.querySelector(".wrapper").appendChild(tempNode);
+            modal.classList.remove("hide-me");
+            // click event to to stope propagation
+            tempNode.addEventListener("click", function(e){ e.stopPropagation(); });
+            // click event to close modal
+            modal.addEventListener("click", function(){ this.classList.add("hide-me") });
         });
     });
-});
+}
 
+function joinUs(){
+    // Filter functionality in drop down menu
 
-// Filter functionality in drop down menu
-var jobSelector = document.querySelector(".job-categories");
-jobSelector.addEventListener("change", function(){
-    document.querySelectorAll(".job").forEach(function(val){
-        if(jobSelector.value == "All Job Category")
-            val.classList.remove("hide-me");
-        else if(jobSelector.value == val.querySelector(".job-cat").getAttribute("data-val"))
-            val.classList.remove("hide-me");
-        else
-            val.classList.add("hide-me");
+    // Listing All job categories
+    var jobSelector = document.querySelector(".job-categories");
+    // Event Listners for each job categories
+    jobSelector.addEventListener("change", function(){
+        document.querySelectorAll(".job").forEach(function(val){
+            // Filter functionality
+            //Filter for all
+            if(jobSelector.value == "All Job Category")
+                val.classList.remove("hide-me");
+            // Filter for sellected
+            else if(jobSelector.value == val.querySelector(".job-cat").getAttribute("data-val"))
+                val.classList.remove("hide-me");
+            // Filter for rest to hide
+            else
+                val.classList.add("hide-me");
+        });
     });
-});
+}
+
+function ourWork(){
+    // Our work Filter functionality
+
+    // defined variables
+    var portfolio_btns = document.querySelectorAll(".portfolio-btn a");
+    var portfolio_list = document.querySelectorAll(".portfolio-item-img")
+    // Click Event on Portfolio btns
+    portfolio_btns.forEach(function(val){
+        val.addEventListener("click", function(){
+            // removing Active class 
+            portfolio_btns.forEach(function(value){value.classList.remove("active");});
+            // adding Active class 
+            val.classList.add("active");
+            var tempStr = val.innerHTML.toLowerCase().replace(" ","-");
+            // Filtering Nodes
+            portfolio_list.forEach(function(Img_Node){
+                if(val.innerHTML=="all")
+                    Img_Node.classList.remove("hide-me")
+                else if( Img_Node.classList.contains(tempStr) )
+                    Img_Node.classList.remove("hide-me");
+                else
+                    Img_Node.classList.add("hide-me");
+            });
+        });
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
