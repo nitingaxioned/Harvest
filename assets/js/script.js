f4ace483992ac68ae038bdbec7f662bbddf4739e
Nitin <niting@axioned.com>
2021-11-11 19:41:22 +0530

    Our work Filter functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,14 +26,35 @@ document.querySelectorAll(".portfolio-item").forEach(function(val){
         modal.classList.remove("hide-me");
         // click event to to stope propagation
         tempNode.addEventListener("click", function(e){ e.stopPropagation(); });
+        // click event to close modal
+        modal.addEventListener("click", function(){ this.classList.add("hide-me") });
     });
 });
-// click event to close modal
-modal.addEventListener("click", function(){ this.classList.add("hide-me") });
 
 
+// Our work Filter functionality
 
-
+var portfolio_btns = document.querySelectorAll(".portfolio-btn a");
+var portfolio_list = document.querySelectorAll(".portfolio-item-img")
+// Click Event on Portfolio btns
+portfolio_btns.forEach(function(val){
+    val.addEventListener("click", function(){
+        // removing Active class 
+        portfolio_btns.forEach(function(value){value.classList.remove("active");});
+        // adding Active class 
+        val.classList.add("active");
+        var tempStr = val.innerHTML.toLowerCase().replace(" ","-");
+        // Filtering Nodes
+        portfolio_list.forEach(function(Img_Node){
+            if(val.innerHTML=="all")
+                Img_Node.classList.remove("hide-me")
+            else if( Img_Node.classList.contains(tempStr) )
+                Img_Node.classList.remove("hide-me");
+            else
+                Img_Node.classList.add("hide-me");
+        });
+    });
+});
 
 
 
