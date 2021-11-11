751975613b889c1ee22621b44b78796e0e5ad582
Nitin <niting@axioned.com>
2021-11-11 17:08:57 +0530

    Modal Box Events added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 /* Author: NDG */
 
+
 // Menu click Events
 
 // click Event on menu icon
@@ -12,7 +13,23 @@ document.querySelector("nav").addEventListener("click", function(){
 document.querySelector(".nav-list").addEventListener("click", function(e){ e.stopPropagation(); });
 
 
+// Latest Work Modal/LightBox
 
+var modal = document.querySelector(".latest-work-modal");
+// click Event on Latest Work list
+document.querySelectorAll(".portfolio-item").forEach(function(val){
+    val.addEventListener("click", function(){
+        modal.querySelector(".wrapper").innerHTML = "";
+        var tempNode = val.querySelector("figure").cloneNode();
+        tempNode.innerHTML = val.querySelector("figure").innerHTML;
+        modal.querySelector(".wrapper").appendChild(tempNode);
+        modal.classList.remove("hide-me");
+        // click event to to stope propagation
+        tempNode.addEventListener("click", function(e){ e.stopPropagation(); });
+    });
+});
+// click event to close modal
+modal.addEventListener("click", function(){ this.classList.add("hide-me") });
 
 
 
